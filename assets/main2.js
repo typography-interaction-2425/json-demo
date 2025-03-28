fe81ecd96d681d1b672bf6a985a30fdb5b6328c6
Eric Li <eric@orchardanalytics.co>
2025-03-27 22:50:40 -0400

    add some basic list behavior

--- assets/main2.js
+++ assets/main2.js
@@ -1,4 +1,4 @@
-document.querySelector('form').onsubmit = (event) => event.preventDefault()
+let names = []
 
 document.querySelector('form').oninput = (event) => {
   let name = document.querySelector('#name').value
@@ -6,4 +6,33 @@ document.querySelector('form').oninput = (event) => {
 
   let color = document.querySelector('#color').value
   document.querySelector('#name-heading').style.color = color
+}
+
+document.querySelector('form').onsubmit = (event) => {
+  event.preventDefault()
+
+  let nameValue = document.querySelector('#name').value
+  let colorValue = document.querySelector('#color').value
+
+  document.querySelector('#name').value = ''
+  document.querySelector('#name-heading').textContent = ''
+
+  let nameObject = {
+    name: nameValue,
+    color: colorValue,
+  }
+  names.push(nameObject)
+  renderNames()
+}
+
+const renderNames = () => {
+  let namesList = document.querySelector('#names')
+  namesList.innerHTML = ''
+
+  names.forEach((nameObject) => {
+    let nameItem = `
+      <li><h2 style="color: ${nameObject.color};">${nameObject.name}</h2></li>
+    `
+    namesList.insertAdjacentHTML('beforeend', nameItem)
+  })
 }
