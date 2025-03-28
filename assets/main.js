49fdf027bcbe901f8c8696f3c25cb038d30b7de8
Eric Li <eric@orchardanalytics.co>
2025-03-27 22:16:43 -0400

    update data.json, and load it onto the page with a conditional

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,34 @@
+let renderItems = (data) => {
+  let containerEl = document.querySelector('#pizza-places')
+
+  data.forEach(item => {
+    let rating = item['Rating']
+    let className = ''
+
+    if (rating === '-') {
+      rating = 'No rating'
+      className = 'no-rating'
+    } else {
+      rating = `${rating}/5`
+    }
+
+    let itemHtml = `
+      <li class="${className}">
+        <h2>${item['Place']}</h2>
+        <p>${item['Location']}</p>
+        <p>${item['Borough']}</p>
+        <p>${rating}</p>
+        <p>${item['Notes']}</p>
+      </li>
+    `
+
+    containerEl.insertAdjacentHTML('beforeend', itemHtml)
+  })
+}
+
 fetch('assets/data.json')
   .then(response => response.json())
   .then(data => {
     // And passes the data to the function, above!
-    console.log(data)
+    renderItems(data)
   })
